b0a21a8185dd7a9274ea6cf9c8d8bd632e087bc2
Ruth J <54176251+RuthJ1811@users.noreply.github.com>
2020-10-12 10:49:53 +0530

    Update queue_event.js

--- queue_event.js
+++ queue_event.js
@@ -1,9 +1,7 @@
 window.addEventListener('message', function (eventData) {
-    console.log("event listener active")
     try {
         if (JSON.parse(eventData.data)) {
             let event = JSON.parse(eventData.data);
-            console.log("Received event");
             if (event.event_code === "custom-event" && event.data && event.data.code === "queue_position_updated") {
                 console.log("Received queued event");
                 const queue_position = event.data.data.queue_position
