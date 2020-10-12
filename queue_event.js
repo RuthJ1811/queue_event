398c799cebe3a5381676eb173456df768c67f462
Ruth J <54176251+RuthJ1811@users.noreply.github.com>
2020-10-12 21:11:47 +0530

    Update queue_event.js

--- queue_event.js
+++ queue_event.js
@@ -3,15 +3,16 @@ window.addEventListener('message', function (eventData) {
         if (JSON.parse(eventData.data)) {
             let event = JSON.parse(eventData.data);
             if (event.event_code === "custom-event" && event.data && event.data.code === "queue_position_updated") {
-                console.log("queued event received");
+                console.log("Received queued event");
                 const queue_position = event.data.data.queue_position;
                 const estimated_time = event.data.data.estimated_time;
-                var yellowFrame = document.getElementById("ymIframe")
-                console.log("ymiframe done")
-                console.log(yellowFrame)
-                yellowFrame.contentWindow.document.querySelector("iframe").querySelector('#circle > div.queue_number').innerHTML = queue_position;
-                yellowFrame.contentWindow.document.querySelector('#estimated_time > div').innerHTML = estimated_time;
-                console.log("updated !")
+                [...document.getElementById("ymIframe").contentWindow.document.querySelectorAll("iframe")]
+                .reverse()[1].contentWindow
+                .postMessage({
+                    queue_message: "queue_message", 
+                    queue_position: queue_position, 
+                    estimated_time: estimated_time 
+                    },"http://127.0.0.1:5500")
             }
         }
     } catch (error) {
