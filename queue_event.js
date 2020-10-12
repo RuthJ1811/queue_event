5d0046355639f25f6118f355d7cabc42d31d2aa9
Ruth J <54176251+RuthJ1811@users.noreply.github.com>
2020-10-12 12:42:14 +0530

    Update queue_event.js

--- queue_event.js
+++ queue_event.js
@@ -6,7 +6,7 @@ window.addEventListener('message', function (eventData) {
                 console.log("Received queued event");
                 const queue_position = event.data.data.queue_position;
                 const estimated_time = event.data.data.estimated_time;
-                var yellowFrame = document.getElementById(“ymIframe”).contentWindow
+                var yellowFrame = document.getElementById("ymIframe").contentWindow
                 let src = yellowFrame.document.querySelector('iframe').src
                 console.log(src)
             }
